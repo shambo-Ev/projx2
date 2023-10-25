d4ed5211998f8249439387bfe9ee430d76ceda80
Shambo <shamim.agahi@hotmail.com>
2023-10-25 13:56:25 +1100

    updated files

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
-
+const fs = require('fs');
+const path = require('path'); // Import the 'path' module
 const app = express();
 const PORT = 3000;
 
@@ -12,6 +13,26 @@ app.get('/proxy', async (req, res) => {
         res.send(response.data);
     } catch (error) {
         res.status(500).send('Error fetching the content');
+    }
+});
+
+app.get('/jsondata', (req, res) => {
+    try {
+        const jsonDataPath = path.join(__dirname, 'package.json'); // Construct the path to package.json
+        const jsonData = fs.readFileSync(jsonDataPath, 'utf8');
+        const parsedData = JSON.parse(jsonData);
+
+        // Access the 'myUrl' property from package.json
+        const myUrl = parsedData.myUrl;
+
+        if (myUrl) {
+            res.send(myUrl);
+        } else {
+            res.status(500).send('No URL found in package.json');
+        }
+    } catch (error) {
+        console.error('Error fetching JSON data:', error);
+        res.status(500).send('Error fetching JSON data');
     }
 });
 
